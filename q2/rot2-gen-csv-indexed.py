89252fc199ff443a884e41d6f4f757f37a92ab19
rishavroy97 <rishavroy008@gmail.com>
2024-11-23 19:06:07 -0500

    Rot2 for MariaDB

--- q2/rot2-gen-csv-indexed.py
+++ q2/rot2-gen-csv-indexed.py
@@ -1,4 +1,19 @@
 import pandas as pd
+import random
+from datetime import datetime, timedelta
 
-df = pd.read_csv("orders.csv")
-df.to_csv("orders-indexed.csv", index_label="id")
+NUM_ROWS = 10_000_000
+
+def generate_indexed_data(csv_file):
+    data = {
+        "customer_id": [random.randint(1, 500) for _ in range(NUM_ROWS)],
+        "order_date": [(datetime.today() - timedelta(days=random.randint(0, 3650))) for _ in range(NUM_ROWS)],
+        "order_amount": [round(random.uniform(10, 1000), 2) for _ in range(NUM_ROWS)]
+    }
+        
+    df = pd.DataFrame(data)
+    df.to_csv(csv_file, index_label="id")
+
+if __name__ == '__main__':
+    csv_file_path = "orders-indexed.csv"
+    generate_indexed_data(csv_file_path)
--- q2/rot2-mariadb.sql
+++ q2/rot2-mariadb.sql
@@ -1,35 +1,49 @@
-CREATE TABLE orders (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
-    order_amount NUMERIC(10, 2)
-);
+-- CREATE TABLE orders (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     customer_id INT,
+--     order_date DATE,
+--     order_amount NUMERIC(10, 2)
+-- );
+
+CREATE TABLE orders (id INT, customer_id INT, order_date DATE, order_amount NUMERIC(10, 2));
 
 -- RULE OF THUMB:
 -- Creating a clustered index on the column that is used in the where clause
 -- improves performance when compared to a Non-clustered index
 -- This is because all the records will probably be on the same page.
 
-LOAD DATA INFILE '/home/rr4577/ads/db-tuning-assignment/q2/orders.csv'
+-- LOAD DATA INFILE '/home/rr4577/ads/db-tuning-assignment/q2/orders.csv'
+-- INTO TABLE orders
+-- FIELDS TERMINATED BY ','
+-- IGNORE 1 LINES
+-- (customer_id, order_date, order_amount);
+
+LOAD DATA INFILE '/home/rr4577/ads/db-tuning-assignment/q2/orders-indexed.csv'
 INTO TABLE orders
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES
-(customer_id, order_date, order_amount);
+(id, customer_id, order_date, order_amount);
 
-CREATE INDEX customer_idx ON orders (customer_id);
+-- CREATE INDEX customer_idx ON orders (customer_id);
+
+-- 2.483 sec
+CREATE INDEX order_idx ON orders (id);
+
 
 -- non-clustered index (147.914 ms)
+-- 1.990 sec
 SET profiling = 1;
-SELECT * FROM orders WHERE customer_id BETWEEN 20 AND 50;
+-- SELECT * FROM orders WHERE customer_id BETWEEN 20 AND 50;
+SELECT * FROM orders where id BETWEEN 200000 AND 600000
 SHOW PROFILES;
 
 -- clustered index (66.661 ms)
-DROP INDEX customer_idx ON orders;
+DROP INDEX order_idx ON orders;
 
-ALTER TABLE orders DROP PRIMARY KEY;
-ALTER TABLE orders ADD PRIMARY KEY (customer_id);
+ALTER TABLE orders ADD PRIMARY KEY (id); -- 4.773 sec
 
-SELECT * FROM orders WHERE customer_id BETWEEN 20 AND 50;
+-- 1.361 sec
+SELECT * FROM orders where id BETWEEN 200000 AND 600000
 
 
 -- Conclusion: Doubles the throughput
